9b0ec0670077630e7e8dd5c03539273723b0a6c3
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-03-01 21:57:24 -0800

    Add test eventually

--- rosette2.test.rkt
+++ rosette2.test.rkt
@@ -43,10 +43,53 @@
     )
   )
 
+(define (test-eventually)
+  (test-case
+    "test-eventually"
+    (define-symbolic sb boolean?)
+
+    (define stream
+      (if sb (list 'b 'b 'b) (list 'b 'b 'a)))
+    (define formula (ltleventually (ltlval 'a)))
+
+    (define sol (solve
+      (assert
+        (equal?
+          (ltleval formula stream)
+          #t)
+        )))
+    (check-true (sat? sol))
+    (check-equal? (evaluate sb sol) #f)
+    )
+  )
+
+(define (test-eventually-nested)
+  (test-case
+    "test-eventually-nested"
+    (define-symbolic sb boolean?)
+
+    (define stream
+      (list 'a (if sb 'b 'a) 'c 'd))
+    (define formula
+      (ltleventually (ltland (ltlval 'b) (ltleventually (ltlval 'd)))))
+
+    (define sol (solve
+      (assert
+        (equal?
+          (ltleval formula stream)
+          #t)
+        )))
+    (check-true (sat? sol))
+    (check-equal? (evaluate sb sol) #t)
+    )
+  )
+
 (module+ test
   (define/provide-test-suite ltl-tests
     (test-next)
     (test-always)
+    (test-eventually)
+    (test-eventually-nested)
     )
   (run-tests ltl-tests)
   )
